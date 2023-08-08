738fe85e239011957c16ed4fedc0dcec79d7f15d
Adem Mohammed <45444428+abulhuman@users.noreply.github.com>
2023-08-08 12:03:05 +0300

    config vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <router-view></router-view>
+  <v-app>
+    <v-toolbar title="Three-way Studio Logistics">
+      <v-btn to="/">Home</v-btn>
+      <v-btn to="/login">Login</v-btn>
+      <v-btn to="/register">Register</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn to="/logout">Logout</v-btn>
+    </v-toolbar>
+
+    <v-main class="mt-3">
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.css';
 
 createApp(App)
-    .use(router)
-    .mount('#app');
+  .use(router)
+  .use(vuetify)
+  .mount('#app');
+
